862400548c56284d749c541318ded899f406ffee
nirmalendu prakash <nirmalenduprakash@nirmalendus-MacBook-Air.local>
2018-08-07 09:27:55 +0530

    expedia

--- nnUtil.py
+++ nnUtil.py
@@ -1,5 +1,6 @@
 import numpy as np
 import h5py
+import pandas as pd
 
 def linearForward(A,w,b):
     z=np.dot(A,w.T)+b.T
@@ -66,3 +67,6 @@ def load_dataset():
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
     
     return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
+
+def load_dataset_csv(file):
+    return pd.read_csv(file)
